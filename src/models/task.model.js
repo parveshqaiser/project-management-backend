e964ef9abbe2e4be28686552f1e881eaa7ac2e7d
Parvesh Qaiser <ahmed75parvesh@gmail.com>
2026-03-18 21:26:47 +0530

    task model added

---
+++ src/models/task.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const TaskSchema = new mongoose.Schema({
+    title : {
+        type : String,
+        required : true
+    },
+    description : {
+        type : String,
+        required : true
+    },
+    taskCategory : {
+        type : String,
+        required : true,
+        enum : ["Bug", "Feature", "Improvement","Other"]
+    },
+    status : {
+        type : String,
+        default: "Pending",
+        enum: ["In Progress", "Review", "Pending", "Completed", "Close", "Deleted"],
+    },
+    priority : {
+        type : String,
+        default: "Medium",
+        enum: ["Low","Medium","High"]
+    },
+    dueDate : {
+        type: Date, 
+        required: true
+    },
+    assignedTo : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+        required: true,
+    },
+    assignedBy : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users",
+        required: true,
+    },
+},{timestamps:true});
+
+let TaskModel = new mongoose.model("tasks", TaskSchema);
+
+export default TaskModel;
