34cc54c6c4f12c1b2d4f8837728a5eb306eca79f
Parvesh Qaiser <ahmed75parvesh@gmail.com>
2026-03-20 11:16:42 +0530

    notes controller added

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -5,7 +5,7 @@ export const createNotes = async(req, res)=>{
     try {
         let currentUser = req.user;
         let {projectId} = req.params;
-        
+
         if(currentUser.userRole !=="admin"){
             return res.status(403).json({message : "You are not Authorized to Create Notes", success: false});
         }
@@ -22,6 +22,78 @@ export const createNotes = async(req, res)=>{
 
         res.status(201).json({message: "Note Created", success : true});
       
+    } catch (error) {
+        res.status(500).json({ message: "Server Error", error: error.message, success: false });
+    }
+}
+
+export const updateNotes = async(req, res)=>{
+
+    try {
+        let currentUser = req.user;
+        let {projectId, noteId} = req.params;
+
+        if(currentUser.userRole !=="admin"){
+            return res.status(403).json({message : "You are not Authorized to Update Notes", success: false});
+        }
+
+        // check validation here of req.body
+        let {title,content} = req.body;
+
+        let note = await NotesModel.findOne({projectId, _id:noteId});
+
+        if(!note){
+            return res.status(404).json({message : "Notes Doesn't Exist", success :false});
+        }
+
+        note.title = title;
+        note.content = content;
+
+        await note.save();
+        res.status(200).json({message: "Note Updated", success : true});
+
+    } catch (error) {
+        res.status(500).json({ message: "Server Error", error: error.message, success: false });
+    }
+}
+
+export const deleteNote = async(req, res)=>{
+    try {
+
+        let currentUser = req.user;
+        let {projectId, noteId} = req.params;
+
+        if(currentUser.userRole !=="admin"){
+            return res.status(403).json({message : "You are not Authorized to Delete Notes", success: false});
+        }
+
+        let note = await NotesModel.findByIdAndDelete(noteId);
+
+        if(!note){
+            return res.status(404).json({message : "Notes Doesn't Exist", success :false});
+        }
+
+        res.status(200).json({message: "Note Deleted", success : true});
+
+    } catch (error) {
+        res.status(500).json({ message: "Server Error", error: error.message, success: false });
+    }
+}
+
+export const getProjectNotes = async(req, res)=>{
+    try {
+        let currentUser = req.user;
+        let {projectId} = req.params;
+
+        let notes = await NotesModel.find({projectId});
+
+        if(!notes){
+            return res.status(404).json({message : "No Notes Found", success: false});
+        }
+
+        res.status(200).json({data : notes , message : "Notes Fetched", success:true});
+
+        
     } catch (error) {
         res.status(500).json({ message: "Server Error", error: error.message, success: false });
     }
--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -1,10 +1,13 @@
 
 import express from "express";
 import { authenticateToken } from "../middlewares/auth.js";
-import { createNotes } from "../controllers/notes.controller.js";
+import { createNotes, getProjectNotes, updateNotes } from "../controllers/notes.controller.js";
 
 const router = express.Router();
 
 router.post("/:projectId", authenticateToken,createNotes);
+router.put("/:projectId/note/:noteId", authenticateToken,updateNotes);
+router.delete("/:projectId/note/:noteId", authenticateToken,updateNotes);
+router.get("/:projectId", authenticateToken,getProjectNotes);
 
 export default router;
